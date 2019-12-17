e9c3ab51f4b9038d3b42bb8c868e5ee6197fa37d
jérôme coupé <jercoupe@gmail.com>
2019-12-17 12:34:03 +0100

    Responsive site banner using picture and source

--- gulp_tasks/createThumbs.js
+++ gulp_tasks/createThumbs.js
@@ -17,48 +17,52 @@ const transforms = [
         src: "src/assets/img/galleries",
         dist: "dist/assets/img/galleries/_600xauto",
         options: {
-            width: 600,
-            resize: "fit",
+            width: 600
         }
     },
     {
         src: "src/assets/img/galleries",
         dist: "dist/assets/img/galleries/_1024xauto",
         options: {
-            width: 1024,
-            resize: "fit",
+            width: 1024
         }
     },
     {
         src: "src/assets/img/news_illu",
         dist: "dist/assets/img/news_illu/_600xauto",
         options: {
-            width: 600,
-            resize: "fit",
+            width: 600
         }
     },
     {
         src: "src/assets/img/news_illu",
         dist: "dist/assets/img/news_illu/_1024xauto",
         options: {
-            width: 1024,
-            resize: "fit",
+            width: 1024
+        }
+    },
+    {
+        src: "src/assets/img/page_banners",
+        dist: "dist/assets/img/page_banners/_600x400",
+        options: {
+            width: 600,
+            height: 400,
+            fit: "cover",
+            position: 'top',
         }
     },
     {
         src: "src/assets/img/page_banners",
         dist: "dist/assets/img/page_banners/_600xauto",
         options: {
-            width: 600,
-            resize: "fit",
+            width: 600
         }
     },
     {
         src: "src/assets/img/page_banners",
         dist: "dist/assets/img/page_banners/_1024xauto",
         options: {
-            width: 1024,
-            resize: "fit",
+            width: 1024
         }
     }
 ];
@@ -66,17 +70,17 @@ const transforms = [
 function createThumbnails(done) {
     transforms.forEach(transform => {
         let globPattern = `${transform.src}/**/*`;
-        let srcFiles = glob.sync(globPattern, {nodir: true});
+        let srcFiles = glob.sync(globPattern, { nodir: true });
         srcFiles.forEach((filePath) => {
             let srcFile = filePath;
             let fileName = path.basename(srcFile);
             let distFile = `${transform.dist}/${fileName}`;
 
-            if(!fs.existsSync(transform.dist)) {
-                fs.mkdirSync(transform.dist, {recursive: true});
+            if (!fs.existsSync(transform.dist)) {
+                fs.mkdirSync(transform.dist, { recursive: true });
             }
 
-            if(!fs.existsSync(distFile)) {
+            if (!fs.existsSync(distFile)) {
                 sharp(srcFile)
                     .resize(transform.options)
                     .toFile(distFile)
